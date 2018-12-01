f02d3ab667adaa0046eaca575af302d24ecf8d80
Piotr Macha <me@piotrmacha.pl>
2018-12-01 15:39:23 +0100

    [Day 1] Change naming to more precise

--- 01_Chronal_Calibration/src/main/scala/pl/piotrmacha/aoc2018/day1/Part2.scala
+++ 01_Chronal_Calibration/src/main/scala/pl/piotrmacha/aoc2018/day1/Part2.scala
@@ -1,8 +1,8 @@
 package pl.piotrmacha.aoc2018.day1
 
 object Part2 {
-  def find(acc: Int, seen: Set[Int], input: Array[Int]): Int = {
-    val value = acc + input.head
+  def find(frequency: Int, seen: Set[Int], input: Array[Int]): Int = {
+    val value = frequency + input.head
     if (seen.contains(value)) value
     else find(value, seen + value, input.tail :+ input.head)
   }
--- 01_Chronal_Calibration/src/test/scala/pl/piotrmacha/aoc2018/day1/Part2Spec.scala
+++ 01_Chronal_Calibration/src/test/scala/pl/piotrmacha/aoc2018/day1/Part2Spec.scala
@@ -3,7 +3,7 @@ package pl.piotrmacha.aoc2018.day1
 import org.scalatest.{FlatSpec, Matchers}
 
 class Part2Spec extends FlatSpec with Matchers {
-  "Part 2" should "return correct frequency" in {
+  "Part 2" should "return repeated frequency" in {
     val changes = Array(1, -2, 3, 1)
     Part2.run(changes) should be (2)
   }
