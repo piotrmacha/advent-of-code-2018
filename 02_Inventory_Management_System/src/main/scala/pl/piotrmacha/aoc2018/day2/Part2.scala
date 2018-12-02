1d4469b56367c4ab5db3a116dc9aa7cc56fb5dcb
Piotr Macha <me@piotrmacha.pl>
2018-12-02 21:29:37 +0100

    [Day 2] Make Part 2 more readable

--- 02_Inventory_Management_System/src/main/scala/pl/piotrmacha/aoc2018/day2/Part2.scala
+++ 02_Inventory_Management_System/src/main/scala/pl/piotrmacha/aoc2018/day2/Part2.scala
@@ -6,17 +6,31 @@ object Part2 {
     else diff(a.tail, b.tail, if (a.head != b.head) carry :+ b.head else carry)
   }
 
+  def diff(a: String, b: String): Array[Char] = diff(a.toCharArray, b.toCharArray)
+
   def intersection(a: Array[Char], b: Array[Char], carry: Array[Char] = Array()): Array[Char] = {
     if (a.isEmpty && b.isEmpty) carry
     else intersection(a.tail, b.tail, if (a.head == b.head) carry :+ a.head else carry)
   }
 
   def run(input: Array[String]): String = {
-    val ids = input.map(id => (id, input.filter(p => p != id)))
-      .map(t => (t._1, t._2.map(pair => diff(pair.toCharArray, t._1.toCharArray))))
-      .filter(t => t._2.exists(diff => diff.length == 1))
-      .map(t => t._1)
-      .map(s => s.toCharArray)
+    val ids = input
+      .map(id => (id, input))
+      .map(t => {
+        val (id, inputs) = t
+        val pairs = inputs
+          .filter(s => s != id)
+          .map(pair => diff(id, pair))
+        (id, pairs)
+      })
+      .filter(t => {
+        val (_, pairs) = t
+        pairs.exists(diff => diff.length == 1)
+      })
+      .map(t => {
+        val (id, _) = t
+        id.toCharArray
+      })
 
     if (ids.length != 2) {
       throw new RuntimeException("Invalid input data, no valid result")
