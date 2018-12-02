f19ed187f9f9409dc8b80c0ce58d5c332861568e
Piotr Macha <me@piotrmacha.pl>
2018-12-02 20:49:23 +0100

    [Day 2] Simplify intersection method

--- 02_Inventory_Management_System/src/main/scala/pl/piotrmacha/aoc2018/day2/Part2.scala
+++ 02_Inventory_Management_System/src/main/scala/pl/piotrmacha/aoc2018/day2/Part2.scala
@@ -6,12 +6,9 @@ object Part2 {
     else diff(a.tail, b.tail, if (a.head != b.head) carry :+ b.head else carry)
   }
 
-  def intersection(input: Array[Array[Char]], carry: Array[Char] = Array()): Array[Char] = {
-    if (input.forall(p => p.isEmpty)) carry
-    else intersection(
-      input.map(p => p.tail),
-      if (input.forall(p => p.head == input.head.head)) carry :+ input.head.head else carry
-    )
+  def intersection(a: Array[Char], b: Array[Char], carry: Array[Char] = Array()): Array[Char] = {
+    if (a.isEmpty && b.isEmpty) carry
+    else intersection(a.tail, b.tail, if (a.head == b.head) carry :+ a.head else carry)
   }
 
   def run(input: Array[String]): String = {
@@ -21,10 +18,10 @@ object Part2 {
       .map(t => t._1)
       .map(s => s.toCharArray)
 
-    if (ids.length < 2) {
+    if (ids.length != 2) {
       throw new RuntimeException("Invalid input data, no valid result")
     }
 
-    intersection(ids).mkString
+    intersection(ids(0), ids(1)).mkString
   }
 }
